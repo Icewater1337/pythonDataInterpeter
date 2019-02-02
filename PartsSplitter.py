fdc42f3a4d632e0881a528d1aa35af959b2bdc32
Mathias Fuchs <fuchsmath@gmail.com>
2019-02-02 12:08:40 +0100

    tmp save

--- PartsSplitter.py
+++ PartsSplitter.py
@@ -4,13 +4,12 @@ import pandas as pd
 from datetime import datetime
 import os
 import dataTimestamper as dst
+import time as time
 
 baseFolder = "C:/Users/Icewater/Google Drive/uni/Informatik/MasterThesis/data/"
 
-#time = datetime.utcfromtimestamp(1545133022).strftime('%d-%m-%Y %H:%M:%S')
+# .strftime('%d-%m-%Y %H:%M:%S')
 
-colnames=['EP','StratFirst','EndFirst', 'StartSecond', 'EndSecond']
-timestamps = pd.read_csv(baseFolder + 'timestamps.csv', names=colnames, header=0, delimiter=';')
 
 all_empatica_data = {}
 # GO through all the folders taht contain empatica
@@ -19,8 +18,50 @@ for directory in [x[0] for x in os.walk(baseFolder)]:
         df = dst.readFiles(directory + "/")
         all_empatica_data[directory.split("/")[-1]] = df
 
-
-
 all_empatica_data
 
-#Dataframe with timestamps
+#
+timestamps = pd.read_csv(baseFolder + 'timestamps.csv', header=0, delimiter=';')
+timestamps = timestamps.drop([9], axis=0)
+# Loop through all dicts and split all at the given timestamps
+for index, row in timestamps.iterrows():
+    episode = "empatica_ep_" + str(row['EP']).zfill(2)
+    current_ep = all_empatica_data[episode]
+    # get the time from the episode
+    current_ep_start = datetime.fromtimestamp(current_ep['HR'].iloc[0,].name)
+    start_first_arr = row['StartFirst'].split(":")
+    start_first = current_ep_start.replace(second=int(start_first_arr[2]), minute=int(start_first_arr[1]),
+                                           hour=int(start_first_arr[0]))
+    end_first_arr = row['EndFirst'].split(":")
+    end_first = current_ep_start.replace(second=int(end_first_arr[2]), minute=int(end_first_arr[1]),
+                                         hour=int(end_first_arr[0]))
+
+    start_second_arr = row['StartSecond'].split(":")
+    start_second = current_ep_start.replace(second=int(start_second_arr[2]), minute=int(start_second_arr[1]),
+                                            hour=int(start_second_arr[0]))
+
+    end_second_arr = row['EndSecond'].split(":")
+    end_second = current_ep_start.replace(second=int(end_second_arr[2]), minute=int(end_second_arr[1]),
+                                          hour=int(end_second_arr[0]))
+
+    for key, value in current_ep.items():
+        #start_idx_first = 0
+        #start_idx_second = 0
+        #end_idx_first = 0
+        #end_idx_second = 0
+        first_part = value.loc[np.logical_and(value.index > time.mktime(start_first.timetuple()),
+                                              value.index < time.mktime(end_first.timetuple()))]
+        second_part = value.loc[np.logical_and(value.index > time.mktime(start_second.timetuple()),
+                                               value.index < time.mktime(end_second.timetuple()))]
+        #for ro_w in value.iterrows():
+            #            date_time = datetime.(ro_w[0])
+
+         #   print(ro_w)
+
+            # date_time = datetime.utcfromtimestamp(ro_w[0])
+
+            # d = datetime.date(2015, 1, 5)
+
+            # unixtime = time.mktime(d.timetuple())
+
+# Dataframe with timestamps
